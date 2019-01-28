5049540f56c0751582fdb8cea7ab1929ea95cbdf
kxdang <ikien.dang@gmail.com>
2019-01-28 18:03:44 -0500

    React JS 56

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -4,10 +4,10 @@ import PropTypes from 'prop-types';
 
 
 class Todos extends Component {
-    render() {
 
+    render() {
         return this.props.todos.map((todo) => (
-            <TodoItem key={todo.id} todo={todo} />
+            <TodoItem key={todo.id} todo={todo} markComplete={this.props.markComplete} delTodo={this.props.delTodo} />
         ));
     }
 }
@@ -15,5 +15,6 @@ class Todos extends Component {
 Todos.propTypes = {
     todos: PropTypes.array.isRequired
 }
+
 
 export default Todos;
